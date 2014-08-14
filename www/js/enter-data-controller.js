5903a5ff35b8f98e8124d12d7d4b26f235c97760
Laura <lauradhamilton@gmail.com>
2014-08-14 13:13:55 -0500

    Adding input fields

--- www/js/enter-data-controller.js
+++ www/js/enter-data-controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('starter.controllers').controller('EnterDataCtrl',
+
   function Ctrl($scope){
     $scope.date = new Date();
+    $scope.cm = ['Sticky','Creamy','Egg White','Watery'];
   }
 );
