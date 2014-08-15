93a7a477ac1bae5773ed8cb555c8b3ce458a414a
Laura <lauradhamilton@gmail.com>
2014-08-15 14:44:28 -0500

    Making the submit button at least do something, even if it's not the right thing.

--- www/js/enter-data-controller.js
+++ www/js/enter-data-controller.js
@@ -2,8 +2,15 @@
 
 angular.module('starter.controllers').controller('EnterDataCtrl',
 
-  function Ctrl($scope){
+  function($scope, $state, $ionicLoading, $firebase, FIREBASE_ROOT, Auth){
     $scope.date = new Date();
     $scope.cm_options = ['Sticky','Creamy','Egg White','Watery'];
+
+    $scope.enter_daily_data = function() {
+     $ionicLoading.show({
+        template: 'Please wait...'
+      });
+      $state.go('home').then($ionicLoading.hide());
+    };
   }
 );
