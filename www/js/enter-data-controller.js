7671e478475a88787e59e0bd2ca8d66b26cc57fb
Laura <lauradhamilton@gmail.com>
2014-08-19 15:55:42 -0500

    Left and right arrows should change the date

--- www/js/enter-data-controller.js
+++ www/js/enter-data-controller.js
@@ -4,14 +4,20 @@ angular.module('starter.controllers').controller('EnterDataCtrl',
 
   function($scope, $stateParams, $q, $state, $ionicLoading, $firebase, $filter, FIREBASE_ROOT, Auth, User, Signup){
     $scope.data_date = new Date($stateParams.date);
+
+    var previous_date = $scope.data_date - 1000*60*60*24;
+    var next_date = $scope.data_date + 1000*60*60*24;
+
     $scope.cm_options = ['Sticky','Creamy','Egg White','Watery'];
 
     $scope.goToPreviousDate = function() {
-      $state.go('home');
+      $state.go('enter-data/:date', {date: previous_date});
+      $stateParams.date = $scope.data_date;
     }
 
     $scope.goToNextDate = function() {
-      $state.go('home');
+      $state.go('enter-data/:date', {date: next_date});
+      $stateParams.date = $scope.data_date
     }
 
     $scope.date = {
