21874ef3266fd56b75f47ec2f2a28d1952aceb57
Laura <lauradhamilton@gmail.com>
2014-08-20 17:27:30 -0500

    Making the initial d3 line graph show up

--- www/js/calendar-controller.js
+++ www/js/calendar-controller.js
@@ -3,12 +3,24 @@
 angular.module('starter.controllers').controller('CalendarCtrl',
 
   function($scope, $q, $state, $ionicLoading, $firebase, FIREBASE_ROOT, Auth, User){
-  $scope.graph = {'width': 265, 'height': 134};
-  $scope.circles = [
-    {'x': 15, 'y': 20, 'r':30},
-    {'x': 35, 'y': 60, 'r':20},
-    {'x': 55, 'y': 10, 'r':40},
-  ]
-}
+    $scope.graph = {'width': 265, 'height': 134};
+    $scope.points = [
+      {'x': 3,  'y': 7 },
+      {'x': 5,  'y': 15},
+      {'x': 7,  'y': 8 },
+      {'x': 11, 'y': 17},
+      {'x': 13, 'y': 13},
+      {'x': 17, 'y': 23}
+    ];
+
+    var x = d3.time.scale().range([0, $scope.graph.width]);
+    var y = d3.scale.linear().range([$scope.graph.height, 0]);
+    x.domain(d3.extent($scope.points, function(d) {return d.x}));
+    y.domain(d3.extent($scope.points, function(d) {return d.y}));
+
+    $scope.line = d3.svg.line()
+      .x(function(d) {return x(d.x);})
+      .y(function(d) {return y(d.y);});
+    }
 );
 
